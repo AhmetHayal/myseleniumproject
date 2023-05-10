311e782536984bb1064e9a6f74a6124019377aed
Ahmet Hayal <theospecter@gmail.com>
2023-05-10 16:40:06 +0300

    10May2

---
+++ src/test/java/com/myfirstproject/practices/practice01/Q04_Calculator.java
@@ -0,0 +1,25 @@
+package com.myfirstproject.practices.practice01;
+
+public class Q04_Calculator {
+    // Create chrome driver by using @Before annotation and WebDriverManager
+    // Navigate to  https://testpages.herokuapp.com/styled/index.html
+    // Click on  Calculator under Micro Apps
+    // Type any number in the first input
+    // Type any number in the second input
+    // Click on Calculate
+    // Get the result
+    // Verify the result
+    // Print the result
+    // Close the browser by using @After annotation
+
+    // Create chrome driver by using @Before annotation and WebDriverManager
+    // Navigate to  https://testpages.herokuapp.com/styled/index.html
+    // Click on  Calculator under Micro Apps
+    // Type any number in the first input
+    // Type any number in the second input
+    // Click on Calculate
+    // Get the result
+    // Verify the result
+    // Print the result
+    // Close the browser by using @After annotation
+}
