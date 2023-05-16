50b27b396ae0f6c4e677b19d2dc1a58362885e4f
Ahmet Hayal <theospecter@gmail.com>
2023-05-16 12:44:01 +0300

    day9-2

--- src/test/java/com/myfirstproject/Day10_ScreenshotsOfEntirePage.java
+++ src/test/java/com/myfirstproject/Day10_ScreenshotsOfEntirePage.java
@@ -1,6 +1,56 @@
 package com.myfirstproject;
 
-public class Day10_ScreenshotsOfEntirePage {
+import com.myfirstproject.utilities.TestBase;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
 
-// CREATE A
+import java.io.IOException;
+
+public class Day10_ScreenshotsOfEntirePage extends TestBase {
+
+//    CREATE A REUSABLE METHOD THAT WILL TAKE A SCREENSHOT IN THE TEST BASE-DONE
+//    Extent Test Base
+//    Copy the code from Day09 Auto Complete
+//    Import the classes if you see any red
+    @Test
+    public void takeScreenshotOfPageTest() throws InterruptedException, IOException {
+//        Given user is on https://testcenter.techproeducation.com/index.php?page=autocomplete
+        driver.get("https://testcenter.techproeducation.com/index.php?page=autocomplete");
+
+//        When user type “uni” in the search box
+        //TAKE SCREENSHOT
+        takeScreenshotOfEntirePage();
+
+        driver.findElement(By.id("myCountry")).sendKeys("uni");
+        Thread.sleep(2000);
+
+//        And select the ‘United Kingdom’ from the suggestions
+        //TAKE SCREENSHOT
+        takeScreenshotOfEntirePage();
+
+        //TAKING SCREENSHOT OF ONLY UNITED KINGDOM ELEMENT
+        takeScreenshotOfThisElement(driver.findElement(By.xpath("//div[@id='myCountryautocomplete-list']//*[.='United Kingdom']")));
+
+        driver.findElement(By.xpath("//div[@id='myCountryautocomplete-list']//*[.='United Kingdom']")).click();
+        Thread.sleep(2000);
+
+//        And click on submit button
+        //TAKE SCREENSHOT
+        //takeScreenshotOfEntirePage();
+
+        takeScreenshotOfEntirePage();
+        driver.findElement(By.xpath(" //input[@type='button']")).click();
+        Thread.sleep(2000);
+        //TAKE SCREENSHOT
+
+//        Then verify the result contains "United Kingdom"
+        //takeScreenshotOfPageTest();
+
+        //TAKING SCREENSHOT OF RESULT ELEMENT
+        takeScreenshotOfThisElement(driver.findElement(By.id("result")));
+
+        Assert.assertTrue(driver.findElement(By.id("result")).getText().contains("United Kingdom"));
+        takeScreenshotOfEntirePage();
+    }
 }
--- src/test/java/com/myfirstproject/utilities/TestBase.java
+++ src/test/java/com/myfirstproject/utilities/TestBase.java
@@ -6,8 +6,15 @@ import org.junit.Before;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
+
 public abstract class TestBase {
     protected static WebDriver driver;
     @Before
@@ -23,15 +30,27 @@ public abstract class TestBase {
         driver.quit();
     }
 
-    public void takeScreenshotOfEntirePage(){
-        //        1. TakeScreenShot class with getScreenShotAs method to capture the screenshot
-
+    public void takeScreenshotOfEntirePage() throws IOException {
+//        1. TakeScreenShot class with getScreenShotAs method to capture the screenshot
+        File image = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 
 //        2. Create a path to save the image
+//          Create a date for giving dynamic name. Otherwise, the screenshots override.
+        String now = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date()); //Getting local date in this format
 //                     CURRENT PROJECT FOLDER         foldername   subfoldername imagename
-        String path = System.getProperty("user.dir")+"/test-output/Screenshots/image.png";
+        String path = System.getProperty("user.dir")+"/test-output/Screenshots/"+now+"image.png";
 
 //        3. Save the image in the path as a file
-
+        FileUtils.copyFile(image, new File(path));
+    }
+/*
+This method captures screenshots of specific elements.
+This method accepts an element and saves the screenshot of that element in the test-output folder
+ */
+    public void takeScreenshotOfThisElement(WebElement element ) throws IOException {
+        File image = element.getScreenshotAs(OutputType.FILE);
+        String now = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+        String path = System.getProperty("user.dir")+"/test-output/ElementScreenshots/"+now+"image.png";
+        FileUtils.copyFile(image, new File(path));
     }
 }
